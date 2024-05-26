655a1105da13e4b5a85bf95dbea1e2ac5c5d8444
Za-Kreative <126286464+Za-Kreative@users.noreply.github.com>
2024-05-26 22:19:46 +0100

    Add files via upload

--- php/edit_task.php
+++ php/edit_task.php
@@ -3,10 +3,10 @@ session_start();
 include 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $user_id = $_SESSION['user_id'];
+    $task_id = $_POST['id'];
     $name = $_POST['name'];
     $description = $_POST['description'];
-    $sql = "INSERT INTO tasks (user_id, name, description, status, removed, created_at) VALUES ('$user_id', '$name', '$description', 'pending', 0, NOW())";
+    $sql = "UPDATE tasks SET name='$name', description='$description' WHERE id='$task_id'";
     $conn->query($sql);
     header('Location: ../index.php');
 }
